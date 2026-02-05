7f1b51574c60b1f418fd89ced765d47dd5231043
Kevin Edenilson <CS23027@ues.edu.sv>
2026-02-04 21:43:47 -0600

    ejercicio9 terminado

---
+++ ejercicio9.py
@@ -0,0 +1,12 @@
+#entrada=input("Digite: ") esto siempre sera un string
+#print(type(entrada))
+mensaje="hay un error"
+
+while mensaje=="hay un error":
+    try:
+        entrada=int(input("Digite la entrada: "))
+        print("-------------------------")
+        print(str(entrada))
+        print("FIN DE LA EJECUCION")
+    except:
+        mensaje="hay un error"
